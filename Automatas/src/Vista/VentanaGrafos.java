6a1816e726d90f46599c203927bfde36b5750575
andro9610 <andro9610@gmail.com>
2019-06-02 13:05:30 -0500

    Update

--- Automatas/src/Vista/VentanaGrafos.java
+++ Automatas/src/Vista/VentanaGrafos.java
@@ -5,6 +5,6 @@ public class VentanaGrafos {
         Ventana recursosGraficos = new Ventana();
         recursosGraficos.crearVentana("Automatas", "../Recursos/iconoAplicacion.png", 800, 600);
         Ventana.ventana.add(recursosGraficos.agregarElementos());
-        Ventana.panel.add(recursosGraficos.crearBoton("Hola mundo", 30, 30, 100, 50));
+        Ventana.panel.add(recursosGraficos.crearBoton("Comprobar", 30, 30, 100, 50));
     }
 }
