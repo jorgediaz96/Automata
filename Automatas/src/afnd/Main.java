53b0a868e0fedae5580949c5b3cd903e9d140c84
jorgediaz96 <pbarragan56@gmail.com>
2019-06-02 11:45:16 -0500

    jj

--- Automatas/src/afnd/Main.java
+++ Automatas/src/afnd/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package launcher;
+package afnd;
 
 /**
  *
@@ -11,7 +11,6 @@ package launcher;
  */
 public class Main {
     public static void main(String[] args) {
-        
-        
+        new pantalla().setVisible(true);
     }
 }
