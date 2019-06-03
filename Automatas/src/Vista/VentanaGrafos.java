dcdf577422293c2af6c7eb8191ba27fc14e3ffd5
jorgediaz96 <pbarragan56@gmail.com>
2019-06-02 22:49:10 -0500

    JTextArea

--- Automatas/src/Vista/VentanaGrafos.java
+++ Automatas/src/Vista/VentanaGrafos.java
@@ -1,11 +1,15 @@
 package Vista;
 
 import Logica.Automata;
+import java.util.TreeSet;
+import Logica.Transformador;
+import javax.swing.JTextArea;
 
 public class VentanaGrafos {
+    
     public void crearVentanaGrafos(){
         Ventana recursosGraficos = new Ventana();
-        Automata z = new Automata();
+        /*Automata z = new Automata();
         Integer numEstados = Integer.parseInt(javax.swing.JOptionPane.showInputDialog("Ingrese número de estados"));
         z.setnumEstados(numEstados);
         z.setEstadoInicial(0);
@@ -19,17 +23,67 @@ public class VentanaGrafos {
         int alf = Integer.parseInt(javax.swing.JOptionPane.showInputDialog("Ingrese cantidad de letras del alfabeto"));
         for (int i = 0; i < alf; i++) {
             z.addLetraAlfabeto(javax.swing.JOptionPane.showInputDialog("Ingrese letra"));
-        }
+        }*/
+        
+        
         recursosGraficos.crearVentana("Automatas", "../Recursos/iconoAplicacion.png", 800, 600);
         Ventana.ventana.add(recursosGraficos.agregarElementos());
         Ventana.panel.add(recursosGraficos.crearTexto("Estados: ", 0, -40, 100, 100));
-        Ventana.panel.add(recursosGraficos.crearTexto(numEstados.toString(), 60, -40, 100, 100));
+        //Ventana.panel.add(recursosGraficos.crearTexto(numEstados.toString(), 60, -40, 100, 100));
         Ventana.panel.add(recursosGraficos.crearTexto("Estados finales: ", 140, -40,300, 100));
-        Ventana.panel.add(recursosGraficos.crearTexto(z.getestadoFinal().toString(), 260, -40, 100, 100));
+        //Ventana.panel.add(recursosGraficos.crearTexto(z.getestadoFinal().toString(), 260, -40, 100, 100));
         Ventana.panel.add(recursosGraficos.crearTexto("Alfabeto: ", 330, -40, 100, 100));
-        Ventana.panel.add(recursosGraficos.crearTexto(z.getAlfabeto().toString(), 400, -40, 100, 100));
+        //Ventana.panel.add(recursosGraficos.crearTexto(z.getAlfabeto().toString(), 400, -40, 100, 100));
         
-        
+        JTextArea jTextArea1 = new JTextArea("Hola",40,400);
+        Ventana.ventana.add(jTextArea1);
+    }
+    
+    public void tabla1(Automata z) {
+        String tabla1 = "\t" + z.getAlfabeto().toString() + "\n";
+
+        for (int i = 0; i < z.getTablaTransiciones().length; i++) {
+            for (int j = 0; j < z.getTablaTransiciones()[i].length; j++) {
+                if (j == 0) {
+                    tabla1 = tabla1 + i + "\t" + (z.getTablaTransiciones()[i][j].toString());
+                } else {
+
+                    tabla1 = tabla1 + (z.getTablaTransiciones()[i][j].toString());
+                }
+            }
+            tabla1 = tabla1 + "\n";
+        }
         
     }
+
+    public void tabla2(Automata trans) {
+        String tabla2 = "\t" + trans.getAlfabeto().toString() + "\n";
+        TreeSet<Integer> table = new TreeSet<Integer>();
+        table.add(0);
+
+        for (int i = 0; i < trans.getTablaTransiciones().length; i++) {
+            for (int j = 0; j < trans.getTablaTransiciones()[i].length; j++) {
+                if (trans.getTablaTransiciones()[i][j].isEmpty()) {
+                    trans.getTablaTransiciones()[i][j] = table;
+                }
+            }
+        }
+
+
+
+        for (int i = 0; i < trans.getTablaTransiciones().length; i++) {
+            for (int j = 0; j < trans.getTablaTransiciones()[i].length; j++) {
+                if (j == 0) {
+                    tabla2 = tabla2 + i + "\t" + (trans.getTablaTransiciones()[i][j].toString());
+                } else {
+
+                    tabla2 = tabla2 + (trans.getTablaTransiciones()[i][j].toString());
+                }
+            }
+            tabla2 = tabla2 + "\n";
+        }
+        
+    }
+
+    
 }
